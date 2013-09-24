7bf102b0ab515c6e565357d5c44b302af8b2f140
Hermann Kaser <hermann.kaser@gmail.com>
2013-09-24 16:49:40 +0100

    Finish exercise 4

---
+++ e4.py
@@ -0,0 +1,23 @@
+#!/bin/env python
+
+# 4. Detect single-character XOR
+#
+# One of the 60-character strings at:
+#
+#  https://gist.github.com/3132713
+#
+# has been encrypted by single-character XOR. Find it. (Your code from
+# 3 should help.)
+
+from lib import find_best_xor_match
+
+matches = []
+for line in open('e4.txt', 'r'):
+    matches.extend(find_best_xor_match(bytes.fromhex(line.rstrip()), 1))
+
+print("Found %d total possible matches" % len(matches))
+
+sorted_matches = sorted(matches, key=lambda s: s['diff'])
+
+for match in sorted_matches[0:40]:
+	print("mask %s: %s; diff %0.4f" % (match['mask'], match['result'], match['diff']))
