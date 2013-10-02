b61c2d17d9cd6f639a6296e09525e2b9d1f52a3f
Hermann Kaser <hermann.kaser@gmail.com>
2013-10-02 21:46:45 +0100

    Finish exercise 7

---
+++ e7.py
@@ -0,0 +1,32 @@
+#!/bin/env python
+
+# 7. AES in ECB Mode
+#
+# The Base64-encoded content at the following location:
+#
+#     https://gist.github.com/3132853
+#
+# Has been encrypted via AES-128 in ECB mode under the key
+#
+#     "YELLOW SUBMARINE".
+#
+# (I like "YELLOW SUBMARINE" because it's exactly 16 bytes long).
+#
+# Decrypt it.
+#
+# Easiest way:
+#
+# Use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+
+import base64
+from Crypto.Cipher import AES
+from Crypto import Random
+
+buff = base64.b64decode(''.join([line.rstrip() for line in open('e7.txt', 'r')]))
+
+key = b'YELLOW SUBMARINE'
+iv = Random.new().read(AES.block_size)
+cipher = AES.new(key, AES.MODE_ECB, iv)
+msg = cipher.decrypt(buff)
+
+print(msg)
